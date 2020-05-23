c325495215081bbd1d3aa95483577dd1f0a6702c
afelix <afelix@aforecoppel.com>
2020-05-22 19:37:00 -0600

    se agrega una pequeña validaciones al formulario a traves de FormGroup y Validators

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,18 +6,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { FormPrincipalComponent } from './form-principal/form-principal.component';
 import { NoEncontradoComponent } from './no-encontrado/no-encontrado.component';
 
-const prefix: String = "retoahorro/";
+const prefix: String = "retoahorro";
 
 const routes: Routes = [// definicion de rutas
-  {
-    path: prefix + 'indexretoahorro', component: FormPrincipalComponent
-  },
-  {
-    path: '', redirectTo: prefix + '/indexretoahorro', pathMatch: 'full'
-  },
-  {
-    path: '**', component: NoEncontradoComponent //cuando una ruta no este definida envia pagina 404
-  }
+  { path: prefix + '/indexretoahorro', component: FormPrincipalComponent },
+  { path: '' + prefix, redirectTo: prefix + '/indexretoahorro', pathMatch: 'full' },
+  { path: '**', component: NoEncontradoComponent }//cuando una ruta no este definida envia pagina 404
 ];
 
 @NgModule({
--- src/app/form-principal/form-principal.component.ts
+++ src/app/form-principal/form-principal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-principal',
@@ -6,6 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./form-principal.component.css']
 })
 export class FormPrincipalComponent implements OnInit {
+
+  formRetoAhorro: FormGroup;
 
   datosForm: any = {
     nombre: "",
@@ -16,16 +19,42 @@ export class FormPrincipalComponent implements OnInit {
     ahorroFijoMensual: "",
     ahorroIncrementalSemanal: ""
   }
-  /* @Input("propiedadForm") datosForm:any; */
-  constructor() {
+
+  constructor(private constructorForm: FormBuilder) {
+
+    this.formRetoAhorro = this.constructorForm.group({
+      txtNombre:
+        [
+          '',//Valor por defecto 
+          [
+            Validators.required,
+            Validators.pattern("[a-zA-Z ]*"),//solo contenga letras o espacios
+            Validators.maxLength(120)
+          ]
+        ],
+      txtEdad: ['', Validators.required],
+      cboFormaCalcular: ['', Validators.required],
+      txtMontoAhorrarAnio: ['', Validators.required],
+      txtMontoFijoSemanal: ['', Validators.required],
+      txtMontoFijoMensual: ['', Validators.required],
+      txtMontoIncremental: ['', Validators.required]
+    });
 
   }
-  
+
+  unirseReto(formRetoAhorro) {
+    console.log(formRetoAhorro);
+
+  }
+  calcularAhorro() {
+    console.log("Calculando ahorros");
+
+  }
+  nuevoCalculo() {
+    console.log("Nuevo Calculo");
+
+  }
+
   ngOnInit() {
-  }
-
-  nuevoCalculo(){
-    console.log("clic nuevo calculo");
-    
   }
 }
